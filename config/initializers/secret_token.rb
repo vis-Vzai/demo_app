8ad1713e6abe14c3879b31ce6584aa148a848328
mld <123456789@qq.com>
2016-10-17 23:31:34 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '0f2a7e776c22eebd96fbdfe972ed67ff6eea72a5aa27220cb3d6e0de1d4c7c5460d117d0fa89cde14d2c622c04b54193490438de1d158b7aa7d93fb89200e07b'
